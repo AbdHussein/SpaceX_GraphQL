7d2f06c33321f68775dcb94f96b0ce52b7bbf091
abddulrahmaanHussein <a.hussein@fis.ps>
2021-10-14 16:54:06 +0300

    deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
-app.use(express.static('public '))
+app.use(express.static('public'))
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public','index.html'))
